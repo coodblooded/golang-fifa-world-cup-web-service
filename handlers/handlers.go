907594f1010ea1cf50d49131c53642d1e51aa935
Sandeep Patel <sandeep@essenvia.com>
2021-08-01 22:33:45 +0530

    update

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -12,6 +12,7 @@ func RootHandler(res http.ResponseWriter, req *http.Request) {
 
 // ListWinners returns winners from the list
 func ListWinners(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
 
 }
 
